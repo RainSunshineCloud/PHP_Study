7c037657caaa54bade4d37ee9c241a7b6a256009
RyanWu <931035553@qq.com>
2018-04-24 15:36:31 +0800

    删除项目

--- jsonp/www.test2.com/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//jsonp 无法使用post请求，只能使用get请求
-
-$arr =['a'=>1,'b'=>2,'c'=>3];
-$str = $_GET['callback'].'('.json_encode($arr).')';
-
-echo $str;
--- swoole/Timer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-swoole_timer_tick(2000,function($timer){
- echo 'dfd\n';
-});
-
-
-
