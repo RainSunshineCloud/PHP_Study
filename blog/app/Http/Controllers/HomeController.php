72aee0d4571a9f6484caf656802742a16995b410
RyanWu <931035553@qq.com>
2018-04-26 18:59:40 +0800

    搭建博客

---
+++ blog/app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Traits\HasRoles;
+
+class HomeController extends Controller
+{
+    use HasRoles;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
